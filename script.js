8c7fb4bb1f023c969f25d0612d9a04c151830b9a
Borys <borisilchenko1991@gmail.com>
2020-07-11 20:05:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let btn1=document.querySelector(".btn1")
+let btn2=document.querySelector(".btn2")
+let btn3=document.querySelector(".btn3")
+let btn4=document.querySelector(".btn4")
+      btn1.onclick=function  (){
+         document.querySelector(".map1").classList.add("show");
+      
+      }
+      let close1= document.querySelector(".close1")
+      close1.onclick=function(){
+         document.querySelector(".map1").classList.remove("show");
+      }
+         btn2.onclick=function  (){
+                  document.querySelector(".map2").classList.add("show");
+               
+               }
+               let close2= document.querySelector(".close2")
+               close2.onclick=function(){
+                  document.querySelector(".map2").classList.remove("show");
+               }
+ 
+               btn3.onclick=function  (){
+                  document.querySelector(".map3").classList.add("show");
+               
+               }
+               let close3= document.querySelector(".close3")
+               close3.onclick=function(){
+                  document.querySelector(".map3").classList.remove("show");
+               }
+               btn4.onclick=function  (){
+                  document.querySelector(".map4").classList.add("show");
+               
+               }
+               let close4= document.querySelector(".close4")
+               close4.onclick=function(){
+                  document.querySelector(".map4").classList.remove("show");
+               }
+   
+
